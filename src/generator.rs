1f3c0921692b90ef5142ba0aa3a11947a0ae9b42
Seungweon Baek <goodbsw@gmail.com>
2026-05-28 16:37:45 -0700

    Update the code to support in-memory producer

--- src/generator.rs
+++ src/generator.rs
@@ -1,39 +1,36 @@
-use tokio::fs::File;
-use tokio::io::{AsyncWriteExt, BufWriter};
 use rand::Rng;
+use tokio::sync::mpsc::Sender;
 
-pub async fn generate_huge_log(file_path: &str, total_lines: usize) {
-    println!("⏳ 딛스크에 {}줄짜리 대용량 테스트 로그 생성 중...", total_lines);
-    
-    // 1. 비동기 쓰기 모드로 파일을 생성하고, 대용량 쓰기에 필수적인 BufWriter를 붙입니다.
-    let file = File::create(file_path).await.unwrap();
-    let mut writer = BufWriter::new(file);
-
-    let mut rng = rand::thread_rng();
+pub async fn generate_logs_to_channel(tx: Sender<String>, counts: usize) {
+    println!("⏳ Generating {} logs", counts);
     
     // 가상의 데이터 풀 (메모리 주소만 참조하여 무작위 로그 생성)
     let methods = ["GET", "POST", "PUT", "DELETE"];
     let statuses = [200, 200, 200, 302, 403, 500, 504]; // 500대 에러 비율 조정
     let paths = ["/api/v1/user", "/api/v1/auth", "/index.html", "/checkout", "/api/v2/bao"];
 
-    for i in 1..=total_lines {
+    for i in 0.. counts {
         // 무작위 데이터 매칭
-        let method = methods[rng.gen_range(0..methods.len())];
-        let status = statuses[rng.gen_range(0..statuses.len())];
-        let path = paths[rng.gen_range(0..paths.len())];
-        let latency = rng.gen_range(15..2500); // 밀리초 단위 레이턴시
-
-        // 💡 Cloudflare Ray ID 시뮬레이션 (16진수 무작위 문자열 생성)
-        let ray_id: u64 = rng.r#gen();
-        
-        // 2. 하나의 완성된 대규모 분산 환경 규격의 로그 포맷 생성
-        let log_line = format!(
-            "CF-RayID: {:x} | [{}] {} -> {} | Status: {} | Latency: {}ms\n",
-            ray_id, method, path, if status >= 500 { "upstream_timeout" } else { "ok" }, status, latency
-        );
+        let log_line = {
+            let mut rng = rand::thread_rng();
+            let method = methods[rng.gen_range(0..methods.len())];
+            let status = statuses[rng.gen_range(0..statuses.len())];
+            let path = paths[rng.gen_range(0..paths.len())];
+            let latency = rng.gen_range(15..2500);
+            let ray_id: u64 = rng.r#gen();
+            
+            // 이 블록의 최종 결과물로 log_line 문자열을 반환합니다 (세미콜론 없음)
+            format!(
+                "CF-RayID: {:x} | [{}] {} -> {} | Status: {} | Latency: {}ms\n",
+                ray_id, method, path, if status >= 500 { "upstream_timeout" } else { "ok" }, status, latency
+            )
+        };
 
         // 3. 비동기 버퍼에 쏟아붓기 (.await로 디스크 커널 이벤트 대기)
-        writer.write_all(log_line.as_bytes()).await.unwrap();
+        if let Err(e) = tx.send(log_line).await {
+            eprintln!("Failed to send logs to the channel: {}", e);
+            continue
+        }
 
         // 10만 줄마다 진행 상황 체크
         if i % 100000 == 0 {
@@ -42,6 +39,6 @@ pub async fn generate_huge_log(file_path: &str, total_lines: usize) {
     }
 
     // 4. 버퍼에 남아있는 찌꺼기 데이터를 디스크에 완전히 밀어 넣고 마칩니다.
-    writer.flush().await.unwrap();
-    println!("✅ 로그 생성 완료: {}", file_path);
+    drop(tx);
+    println!("✅ Log generation is complete");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
 mod generator;
 mod parser;
 use parser::run_log_parser;
-use tokio::io::{AsyncBufReadExt, BufReader};
-use tokio::fs::File;
 use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (tx, rx) = mpsc::channel::<String>(10_000);
-    let file_path = "cloudflare_huge.log";
-    let file = File::open(file_path).await.expect("Failed to open file");
-    let mut reader = BufReader::new(file);
     let parse_handle = tokio::spawn(run_log_parser(rx));
+    let produce_handle = tokio::spawn(generator::generate_logs_to_channel(tx, 1_000_000));
 
-    loop {
-        // producer
-        let mut line = String::new();
-        let byte_read = reader.read_line(&mut line).await.expect("Failed to read bytes from buffer");
-
-
-        if byte_read == 0 {
-            break
-        }
-
-        if let Err(e) = tx.send(line).await {
-            eprintln!("Non-critical: Failed to send log line: {}", e);
-            continue;
-        }
-    }
-    drop(tx);
-    parse_handle.await.expect("Failed to run tasks");
+    let _ = produce_handle.await.expect("Failed to run producer");
+    let _ = parse_handle.await.expect("Failed to run tasks");
     
     Ok(())
 }
